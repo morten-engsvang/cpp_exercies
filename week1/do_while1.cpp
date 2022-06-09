a1bab6d642d5c2800191448fce940bc47d94344e
engsvangmorten <engsvang.morten@gmail.com>
2022-06-09 13:42:33 +0200

    week1 exercise 7

---
+++ week1/do_while1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main()
+{
+   int i = 0;
+
+   do
+   {
+      ++i;
+   }
+   while(i>0);
+
+   std::cout << i << std::endl;
+   return 0;
+}
---
+++ week1/do_while2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main()
+{
+   int i = 0;
+
+   while(i>-1)
+   {
+      ++i;
+   }
+
+   std::cout << i << std::endl;
+   return 0;
+}
